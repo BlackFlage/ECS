76f7241babd6273a0fb3d26c379766dcb7c97bb6
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-11-30 19:54:42 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/ECS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECS.dir/main.cpp.obj"
+  "ECS.exe"
+  "ECS.exe.manifest"
+  "ECS.lib"
+  "ECS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ECS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
