2262cb97be8376d104e97c33a71e01f3e4497606
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-12-01 23:37:54 +0100

    Fixed ComponentArray class with all function declarations in hpp file for templates use.

--- ComponentArray.hpp
+++ ComponentArray.hpp
@@ -7,19 +7,67 @@
 
 #include <unordered_map>
 #include "EntityManager.hpp"
+#include <cassert>
 
-template<typename T>
-class ComponentArray
+class IComponentArray
 {
 public:
-    ComponentArray();
+    virtual ~IComponentArray() = default;
+    virtual void on_entity_destroyed(const Entity& a_entity) = 0;
+};
 
-    void insert_data(const Entity& a_entity, T a_component);
-    void remove_data(const Entity& a_entity);
+template<typename T>
+class ComponentArray : public IComponentArray
+{
+public:
+    ComponentArray()
+    {
+        m_current_size = 0;
+    }
 
-    T& get_data(const Entity& a_entity);
+    void insert_data(const Entity& a_entity, T a_component)
+    {
+        assert(m_entity_to_index.find(a_entity) == m_entity_to_index.end());
 
-    void on_entity_destroyed(const Entity& a_entity);
+        size_t new_index = m_current_size;
+        m_entity_to_index[a_entity] = new_index;
+        m_index_to_entity[new_index] = a_entity;
+        m_component_array[new_index] = a_component;
+        m_current_size++;
+    }
+    void remove_data(const Entity& a_entity)
+    {
+        assert(m_entity_to_index.find(a_entity) != m_entity_to_index.end());
+
+        size_t index_of_removed_entity = m_entity_to_index[a_entity];
+
+        m_component_array[index_of_removed_entity] = m_component_array[m_current_size - 1];
+
+        m_entity_to_index[m_current_size - 1] = index_of_removed_entity;
+        m_index_to_entity[index_of_removed_entity] = m_current_size - 1;
+
+        m_entity_to_index.erase(a_entity);
+        m_index_to_entity.erase(m_current_size - 1);
+
+        --m_current_size;
+    }
+
+    T& get_data(const Entity& a_entity)
+    {
+        assert(m_entity_to_index.find(a_entity) != m_entity_to_index.end());
+
+        size_t index_of_component = m_entity_to_index[a_entity];
+
+        return m_component_array[index_of_component];
+    }
+
+    void on_entity_destroyed(const Entity& a_entity) override
+    {
+        if(m_entity_to_index.find(a_entity) != m_entity_to_index.end())
+        {
+            remove_data(a_entity);
+        }
+    }
 private:
     std::array<T, MAX_ENTITIES> m_component_array;
     std::unordered_map<Entity, size_t> m_entity_to_index;
